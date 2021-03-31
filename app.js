ac8c547f6fcb473f3a6c34b64ed15f1c1af151f9
00010863 <73020836+00010863@users.noreply.github.com>
2021-03-31 02:19:26 -0700

    archiving operations

--- app.js
+++ app.js
@@ -1,111 +1,165 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const fs = require('fs')
-const { Validator } = require('node-input-validator');
+const fs = require("fs");
+const { Validator } = require("node-input-validator");
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use('/static', express.static('public'))
-app.use(express.urlencoded({ extended: false }))
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
-app.get('/create', (req, res) => {
-    res.render('create')
-})
+app.get("/create", (req, res) => {
+    res.render("create");
+});
 
-app.post('/create', (req, res) => {
-    const title = req.body.title
-    const description = req.body.description
-    const datetime = req.body.datetime
+app.post("/create", (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const datetime = req.body.datetime;
 
     //input validation with library NIV
     const v = new Validator(req.body, {
-        title: 'required|minLength:5',
-        description: 'maxLength:100',
-        datetime: 'required'
+        title: "required|minLength:5",
+        description: "maxLength:100",
+        datetime: "required",
     });
 
     v.check().then((matched) => {
         if (!matched) {
-            res.status(422).render('create', { error: v.errors.title.message });
+            res.status(422).render("create", { error: v.errors.title.message });
             // res.status(422).send(v.errors);
         } else {
-            fs.readFile('./data/tasks.json', function (err, data) {
-                if (err) throw err
+            fs.readFile("./data/tasks.json", function (err, data) {
+                if (err) throw err;
 
-                const tasks = JSON.parse(data)
-
+                const tasks = JSON.parse(data);
 
                 tasks.push({
                     id: id(),
                     title: title,
                     datetime: datetime,
                     description: description,
-                })
+                });
 
-                fs.writeFile('./data/tasks.json', JSON.stringify(tasks), err => {
-                    if (err) throw err
+                fs.writeFile("./data/tasks.json", JSON.stringify(tasks), (err) => {
+                    if (err) throw err;
 
-                    res.render('create', { success: true })
-                })
-            })
+                    res.render("create", { success: true });
+                });
+            });
         }
     });
-})
+});
 
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
+    fs.readFile("./data/tasks.json", (err, data) => {
+        if (err) throw err;
 
-    fs.readFile('./data/tasks.json', (err, data) => {
-        if (err) throw err
+        const tasks = JSON.parse(data);
 
-        const tasks = JSON.parse(data)
+        let task = tasks.filter((task) => !task.saved);
 
-        res.render('tasks', { tasks: tasks })
-    })
+        res.render("tasks", { tasks: task });
+    });
+});
 
-})
+app.get("/tasks/:id", (req, res) => {
+    const id = req.params.id;
 
-app.get('/tasks/:id', (req, res) => {
-    const id = req.params.id
+    fs.readFile("./data/tasks.json", (err, data) => {
+        if (err) throw err;
 
-    fs.readFile('./data/tasks.json', (err, data) => {
-        if (err) throw err
+        const tasks = JSON.parse(data);
 
-        const tasks = JSON.parse(data)
+        const task = tasks.filter((task) => task.id == id)[0];
 
-        const task = tasks.filter(task => task.id == id)[0]
+        res.render("detail", { task: task });
+    });
+});
 
-        res.render('detail', { task: task })
-    })
-})
+// app.delete('/tasks/:id/delete', function(req, res) {
 
-// app.delete('/tasks/:id/delete', function (req, res) {
-
-//      const id = parseInt(req.params.id)
-
-//  fs.writeFile('./data/tasks.json', JSON.stringify(tasks), (err, data) => {
-//    if (err) throw err
-
+//     const id = req.params.id
 //     const tasks = JSON.parse(data)
 //     const index = tasks.findIndex((e) => e.id == id)
 //     tasks.splice(index, 1)
+
+//  fs.writeFile('./data/tasks.json', JSON.stringify(tasks), (err) => {
+//    if (err) throw err
+
 //     res.render('tasks', { tasks: tasks})
 //  })
 
 // })
 
-app.listen(8200, err => {
-    if (err) console.log(err)
+//archive
 
-    console.log('Server is running on port 8200..')
-})
+app.get("/tasks/:id/saved", function (req, res) {
+    fs.readFile("./data/tasks.json", (err, data) => {
+        if (err) throw err;
 
+        const id = req.params.id;
+        const tasks = JSON.parse(data);
+        let index = tasks.findIndex((e) => e.id == id);
+        tasks[index].saved = true;
+        let savedTasks = [];
+        savedTasks = tasks.filter((task) => task.saved === true);
+        res.render("saved", { tasks: savedTasks });
+
+        fs.writeFile("./data/tasks.json", JSON.stringify(tasks), (err) => {
+            if (err) throw err;
+
+            res.render("saved", { tasks: savedTasks });
+        });
+    });
+});
+
+app.get("/tasks/:id/unsave", function (req, res) {
+    const id = req.params.id;
+
+    fs.readFile("./data/tasks.json", function (err, data) {
+        if (err) throw err;
+
+        const tasks = JSON.parse(data);
+        let index = tasks.findIndex((e) => e.id == id);
+        tasks[index].saved = false;
+        let unsavedTasks = [];
+        unsavedTasks = tasks.filter((task) => task.saved === false);
+
+    fs.writeFile("./data/tasks.json", JSON.stringify(tasks), (err) => {
+         if (err) throw err;
+
+         res.render("tasks", { tasks: unsavedTasks });
+        });
+    });
+});
+
+app.get("/saved", (req, res) => {
+    fs.readFile("./data/tasks.json", (err, data) => {
+        if (err) throw err;
+
+        const tasks = JSON.parse(data);
+        let savedTasks = [];
+        savedTasks = tasks.filter((task) => task.saved === true);
+
+        fs.writeFile("./data/tasks.json", JSON.stringify(tasks), (err) => {
+            if (err) throw err;
+            res.render("saved", { tasks: savedTasks });
+        });
+    });
+});
+
+app.listen(8200, (err) => {
+    if (err) console.log(err);
+
+    console.log("Server is running on port 8200..");
+});
 
 function id() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return "_" + Math.random().toString(36).substr(2, 9);
 }
-
